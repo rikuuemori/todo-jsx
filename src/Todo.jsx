ab4728789bbd45f35bce663db9d30019e4183d4a
Riku Uemori <epicture7@gmail.com>
2025-03-09 10:38:47 +0900

    作成準備

---
+++ src/Todo.jsx
@@ -0,0 +1,5 @@
+import './index.css';
+
+export const Todo = () => {
+  return <div>hoge</div>
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { Todo } from './Todo';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <strictMode>
+    <Todo />
+  </strictMode>
+);
