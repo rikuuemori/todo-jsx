af5c026bccfd53d3fd5708ba53893b016b0077e0
Riku Uemori <epicture7@gmail.com>
2025-03-09 11:30:49 +0900

    作成準備

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { Todo } from './Todo';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Todo } from "./Todo";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
-  <strictMode>
+  <StrictMode>
     <Todo />
-  </strictMode>
+  </StrictMode>
 );
